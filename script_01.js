36c92334084b162967ccbb7f4e50112bd3276225
Yasmeen <yasmeenmas@gmx.de>
2023-07-13 11:01:00 +0200

    03 Deklaration + Wertzuweisung II

--- script_01.js
+++ script_01.js
@@ -15,12 +15,29 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-let firstName; // Deklaration ( Definition)
-firstName = "Max";  // Wertzuweisung
-console.log(firstName);  // Ausgabe
+// let firstName; // Deklaration ( Definition)
+// firstName = "Max";  // Wertzuweisung
+// console.log(firstName);  // Ausgabe
 
-let familyName;
-familyName = "Mütze";
-console.log(familyName);
+// let familyName;
+// familyName = "Mütze";
+// console.log(familyName);
 
 
+/***** 03 Deklaration + Wertzuweisung II *******/
+
+// let firstName, familyName;
+
+// firstName = prompt("Bitte Vorname eingeben:");
+// familyName = prompt("Bitte Nachname eingeben:");
+
+// console.log(firstName + " " + familyName);
+
+/* JS ist eine untypisierte Sprache! | untyped */
+let test;
+test = "Maxine";
+test = 2; 
+test = false;
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
